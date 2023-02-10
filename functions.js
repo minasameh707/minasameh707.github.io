b52716ef1b27f7e2189b5ccba23aa34c4ed20a52
minasameh707 <minasameh189@gmail.com>
2023-02-10 16:37:18 +0200

    adding new files

---
+++ functions.js
@@ -0,0 +1,25 @@
+
+//how to make search sugesstion?
+var people = ['Steven', 'Sean', 'Stefan', 'Sam', 'Nathan'];
+
+function matchPeople(input) {
+  var reg = new RegExp(input.split('').join('\\w*').replace(/\W/, ""), 'i');
+  return people.filter(function(person) {
+    if (person.match(reg)) {
+      return person;
+    }
+  });
+}
+
+function changeInput(val) {
+  var autoCompleteResult = matchPeople(val);
+  document.getElementById("result").innerHTML = autoCompleteResult;
+}
+
+
+
+
+//Source: https://stackoverflow.com/questions/11404855
+
+
+
